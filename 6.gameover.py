aac2cc04b9be7b79d53c6f6bd5be1ff7e08057dd
SHK4869 <sayyed.humaira@gmail.com>
2018-02-13 16:58:24 +0530

    Add files via upload
    My bad, I forgot to REMOVE convert_alpha() for the score digits and substitue it with convert() instead, birds and pipes ke liye nikala tha but numbers ke liye reh gaya...

--- 6.gameover.py
+++ 6.gameover.py
@@ -32,17 +32,17 @@ def main():
         IMAGES['background'] = pygame.image.load(BACKGROUND).convert()
     	# numbers sprites for score display
     	IMAGES['numbers'] = (
-        pygame.image.load('assets/sprites/0.png').convert_alpha(),
-        pygame.image.load('assets/sprites/1.png').convert_alpha(),
-        pygame.image.load('assets/sprites/2.png').convert_alpha(),
-        pygame.image.load('assets/sprites/3.png').convert_alpha(),
-        pygame.image.load('assets/sprites/4.png').convert_alpha(),
-        pygame.image.load('assets/sprites/5.png').convert_alpha(),
-        pygame.image.load('assets/sprites/6.png').convert_alpha(),
-        pygame.image.load('assets/sprites/7.png').convert_alpha(),
-        pygame.image.load('assets/sprites/8.png').convert_alpha(),
-        pygame.image.load('assets/sprites/9.png').convert_alpha()
-        )    
+        pygame.image.load('assets/sprites/0.png').convert(),
+        pygame.image.load('assets/sprites/1.png').convert(),
+        pygame.image.load('assets/sprites/2.png').convert(),
+        pygame.image.load('assets/sprites/3.png').convert(),
+        pygame.image.load('assets/sprites/4.png').convert(),
+        pygame.image.load('assets/sprites/5.png').convert(),
+        pygame.image.load('assets/sprites/6.png').convert(),
+        pygame.image.load('assets/sprites/7.png').convert(),
+        pygame.image.load('assets/sprites/8.png').convert(),
+        pygame.image.load('assets/sprites/9.png').convert()
+        )        
 
 	IMAGES['player'] = (
             pygame.image.load(PLAYERS_LIST[0]).convert(),
